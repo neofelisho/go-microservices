1ba8ff1ec6a6801107c43e8ffbb94241f07b3f25
Neo Ho <neofelisho@gmail.com>
2020-05-26 13:58:57 +0700

    Manually deploy to EKS. (#20)

---
+++ k8s/update_secret.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NAMESPACE=go-micro-service
+
+kubectl -n $NAMESPACE delete secret gms-database
+kubectl -n $NAMESPACE create secret generic gms-database \
+  --from-literal=HOST="postgres" \
+  --from-literal=PORT="5432" \
+  --from-literal=USER="postgres" \
+  --from-literal=PASSWORD="postgres"
+
+kubectl -n $NAMESPACE delete secret gms-grpc
+kubectl -n $NAMESPACE create secret generic gms-grpc \
+  --from-literal=HOST="grpc-server" \
+  --from-literal=PORT="50051" \
+  --from-literal=TARGET_PORT="50051"
+
+kubectl -n $NAMESPACE delete secret gms-api
+kubectl -n $NAMESPACE create secret generic gms-api \
+  --from-literal=PORT="1323" \
+  --from-literal=TARGET_PORT="1323"
